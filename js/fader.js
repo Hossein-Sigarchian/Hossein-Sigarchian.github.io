3143650acb6559b1faedef7c2fdb5207da67aaad
Hosein_Sigarchian <149937708+Hossein-Sigarchian@users.noreply.github.com>
2024-04-04 16:12:33 +0330

    sedi-website-upload

---
+++ js/fader.js
@@ -0,0 +1,20 @@
+function fadeInPage() {
+    if (!window.AnimationEvent) { return; }
+    var fader = document.getElementById('fader');
+fader.classList.add('fade-out');
+}
+fadeInPage();
+
+window.onscroll = function() {
+    scroll()
+};
+
+function scroll() {
+    if (document.body.scrollTop > 100 ||
+            document.documentElement.scrollTop > 100) {
+        document.getElementById("scroller").style.display = "flex";
+    }
+    else {
+        document.getElementById("scroller").style.display = "none";
+    }
+}
