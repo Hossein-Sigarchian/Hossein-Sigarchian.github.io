fd84d7bb0895734935b635ead7bbd91a93ed1b6f
Hosein_Sigarchian <149937708+Hossein-Sigarchian@users.noreply.github.com>
2024-04-06 22:39:17 +0330

    iframe-height

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function fadeInPage() {
     if (!window.AnimationEvent) { return; }
     var fader = document.getElementById('fader', 'scroller');
@@ -18,3 +20,16 @@ function scroll() {
         document.getElementById("scroller").style.display = "none";
     }
 }
+function illustration() {
+    let link_1 = document.getElementById("link-1")
+    document.getElementById('idIframe').height = "3900px";
+} 
+function Graphic() {
+    document.getElementById('idIframe').height = "3000px";
+    } 
+function photo() {
+    document.getElementById('idIframe').height = "1220px";
+    } 
+function portrait() {
+    document.getElementById('idIframe').height = "1000px";
+    } 
