d2ebe1ee9f488b2df7f93f860299d3ffc236ac30
Hosein_Sigarchian <149937708+Hossein-Sigarchian@users.noreply.github.com>
2024-04-09 17:33:40 +0330

    fonts

--- js/portfolio.js
+++ js/portfolio.js
@@ -22,22 +22,7 @@ function scroll() {
 function iframeLoaded() {
     var iFrameID = document.getElementById('idIframe');
     if(iFrameID) {
-          // here you can make the height, I delete it first, then I make it again
           iFrameID.height = "";
           iFrameID.height = iFrameID.contentWindow.document.body.scrollHeight + "px";
     }   
 }
-
-// function illustration() {
-//     let link_1 = document.getElementById("link-1")
-//     document.getElementById('idIframe').height = "3900px";
-// } 
-// function Graphic() {
-//     document.getElementById('idIframe').height = "3000px";
-//     } 
-// function photo() {
-//     document.getElementById('idIframe').height = "1220px";
-//     } 
-// function portrait() {
-//     document.getElementById('idIframe').height = "1000px";
-//     } 
